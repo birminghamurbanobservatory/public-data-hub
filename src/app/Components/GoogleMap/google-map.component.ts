47bf4fd95ad698b07ae900b0405562cb4d8e9999
Graham Fowler <g.fowler@bham.ac.uk>
2020-04-03 16:30:52 +0100

    Updates center to Behaviour subject so can change map center when user clicks marker.

--- src/app/Components/GoogleMap/google-map.component.ts
+++ src/app/Components/GoogleMap/google-map.component.ts
@@ -1,13 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { GoogleMapService } from './google-map.service';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
 
 @Component({
     selector: 'buo-google-map',
     template: `
         <div class="relative h-full w-full z-10">
 
-            <google-map height="100%" width="100%" [zoom]="zoom" [center]="center">
+            <google-map height="100%" width="100%" [zoom]="zoom" [center]="center$ | async">
 
                 <map-marker 
                     *ngFor="let marker of markers$ | async" 
@@ -25,8 +25,20 @@ import { Observable } from 'rxjs';
 
 export class GoogleMapComponent implements OnInit {
 
+    /**
+     * Map zoom value
+     */
     public zoom = 11;
-    public center = { lat: 52.480100, lng: -1.896478 };
+
+    /**
+     * Map center co-ordinates
+     * As BS so can change map center when marker clicked
+     */
+    public center$ = new BehaviorSubject({ lat: 52.480100, lng: -1.896478 });
+
+    /**
+     * Observable array of markers
+     */
     public markers$: Observable<any>;
 
     constructor(private googleMapService: GoogleMapService) { }
@@ -35,7 +47,22 @@ export class GoogleMapComponent implements OnInit {
         this.markers$ = this.googleMapService.mapMarkers;
     }
 
+    /**
+     * Click event listener for the map markers
+     * 
+     * @param marker : clicked map marker
+     */
     public markerClicked(marker) {
+        this.setMapCenter(marker);
         this.googleMapService.selectMarker(marker);
+    }
+
+    /**
+     * Updates the center point of the map based on the click marker position
+     *
+     * @param marker : map marker
+     */
+    private setMapCenter(marker) {
+        this.center$.next(marker.position);
     }
 }
