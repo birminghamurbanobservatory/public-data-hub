119bef15cf93891cd025fcd72cecb4ca361bc7cd
Graham Fowler <g.fowler@bham.ac.uk>
2020-05-04 17:20:41 +0100

    Adds new methods for getting timeseries data.

--- src/app/Services/timeseries/timeseries.service.ts
+++ src/app/Services/timeseries/timeseries.service.ts
@@ -3,6 +3,8 @@ import { HttpClient } from '@angular/common/http';
 
 import { environment } from './../../../environments/environment';
 import { ApiFunctionsService } from '../../shared/api-functions';
+import { map } from 'rxjs/operators';
+import { Collection } from 'src/app/shared/collection';
 
 @Injectable({
     providedIn: 'root'
@@ -14,10 +16,64 @@ export class TimeSeriesService {
         private apiFunctions: ApiFunctionsService,
     ) {}
 
-    public getTimeSeries(where?: Object) {
+    /**
+     * Retrieve an individual timeseries by Id
+     * 
+     * @param id : timeseries identifier
+     */
+    public getTimeseriesById(id) {
+
+        return this.http.get(`${environment.apiUrl}/timeseries/${id}`)
+
+    }
+
+    /**
+     * Retrieve one or more timeseries by query params
+     * 
+     * @param where : query object
+     */
+    public getTimeSeriesByQuery(where?: Object) {
 
         const qs = this.apiFunctions.whereToQueryString(where);
 
         return this.http.get(`${environment.apiUrl}/timeseries${qs}`)
+        .pipe(
+            map((timeseries: Collection) => {
+                return {
+                    data: timeseries.member.map(this.transformTimeseries),
+                    meta: timeseries.meta,
+                }
+            })
+        )
+
     }
+
+    /**
+     * Retrieve observations for a timeseries
+     * 
+     * @param id : timeseries identifier
+     */
+    public getTimeseriesObservations(id: string) {
+
+        return this.http.get(`${environment.apiUrl}/timeseries/${id}/observations`)
+        .pipe(
+            map((response) => response['member'].map(this.transformReading))
+        )
+
+    }
+
+    private transformTimeseries(series) {
+        return {
+            id: series['@id'],
+            ...series,
+        };
+    }
+
+    private transformReading(reading) {
+        return {
+            id: reading['@id'],
+            ...reading,
+        };
+    }
+
 }
