115ce97889f46be526f6c6a0497366d026d6f06e
SiBell <SiBellUK@gmail.com>
2020-05-20 18:30:40 +0100

    platform makers now circles

--- src/app/Services/map-pins/map-pin.service.ts
+++ src/app/Services/map-pins/map-pin.service.ts
@@ -48,11 +48,11 @@ export class MapPinService {
         let colour = this.colours.generateHexColour(platform.inDeployment)
 
         const icon = {
-            path: 'M12.5,0C5.6,0,0,4.9,0,10.9c0,7.4,11.2,18.3,11.7,18.8c0.4,0.4,1.2,0.4,1.7,0C13.8,29.2,25,18.3,25,10.9C25,4.9,19.4,0,12.5,0z M12.6,14.8c-3,0-5.4-2.2-5.4-4.8s2.4-4.7,5.4-4.7s5.4,2.2,5.4,4.8S15.7,14.8,12.6,14.8z',
+            path: 'M-15,0a15,15 0 1,0 30,0a15,15 0 1,0 -30,0',
             fillColor: colour,
             fillOpacity: 1,
-            strokeWeight: .8,
-            scale: 1
+            strokeWeight: 3,
+            scale: 0.7
         };
 
 
