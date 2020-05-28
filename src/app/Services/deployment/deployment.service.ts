d51e765635a6c9751414df5bc04c8abd6c280baa
Graham Fowler <g.fowler@bham.ac.uk>
2020-05-28 11:43:23 +0100

    Code tidy up for deployment legend solution.

--- src/app/Services/deployment/deployment.service.ts
+++ src/app/Services/deployment/deployment.service.ts
@@ -1,7 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+import { map } from 'rxjs/operators';
+
+import { ColourService } from '../colours/colour.service';
+
 import { environment } from './../../../environments/environment';
+import { Deployment } from 'src/app/Interfaces/deployment.interface';
+import { Collection } from 'src/app/shared/collection';
 
 @Injectable({
     providedIn: 'root'
@@ -10,9 +16,24 @@ export class DeploymentService {
 
     constructor(
         private http: HttpClient,
+        private colours: ColourService,
     ) {}
 
     public getDeployments() {
-        return this.http.get(`${environment.apiUrl}/deployments`)
+        return this.http
+            .get(`${environment.apiUrl}/deployments`)
+            .pipe(
+                map((response: Collection) => response.member),
+                map(items => items.map(item => this.transform(item))),
+            )
+    }
+
+    private transform(item: Deployment): Deployment {
+        const colour = this.colours.generateHexColour(item['@id']);
+
+        return Object.assign(item, {
+            id: item['@id'],
+            colour,
+        });
     }
 }
