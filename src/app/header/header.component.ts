fc40b1e5d8f0df11f0c0c384f6131b4b04ae2978
Graham Fowler <g.fowler@bham.ac.uk>
2020-06-30 13:40:00 +0100

    Fixes issue #72.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -5,17 +5,12 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
 
-  constructor() { }
+  public mobileOpen: Boolean = false;
 
-  ngOnInit(): void {
-  }
-
-  public navDroppedDown = false;
-
-  toggleNavDropdown() {
-    this.navDroppedDown = !this.navDroppedDown;
+  toggleNav() {
+    this.mobileOpen = !this.mobileOpen;
   }
 
 }
