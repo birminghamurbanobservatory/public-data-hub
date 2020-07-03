c58fd1bbadc3eeecd0bc6e0cc79dfd8d937f7092
SiBell <SiBellUK@gmail.com>
2020-07-03 21:07:36 +0100

    consistent footer added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ export const environment = {
   production: true,
   apiUrl: 'https://api.birminghamurbanobservatory.com',
   // The following key should be for production only. On the GCP console you should restrict this key to just the domains (*.birminghamurbanobservatory.com/*) where it will be used. Also restrict it to just the Maps JavaScript API (and perhaps also the Geocoding API, which you may need to enable first under "Enable APIs and Services").
-  googleMapsApiKey: 'AIzaSyDS-Evzqr-4IPzGWflXDDafglhHV-KTYZQ'
+  googleMapsApiKey: 'AIzaSyDS-Evzqr-4IPzGWflXDDafglhHV-KTYZQ',
+  version: require('../../package.json').version
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,8 @@ export const environment = {
   production: false,
   apiUrl: 'https://api.birminghamurbanobservatory.com',
   // The following key is for development only
-  googleMapsApiKey: 'AIzaSyDOT34BxSKrFJc4kN6fATO2VZebVOeCGo4'
+  googleMapsApiKey: 'AIzaSyDOT34BxSKrFJc4kN6fATO2VZebVOeCGo4',
+  version: require('../../package.json').version
 };
 
 /*
