6da05cd34fc9e11c1af485636d1a9ec4f1ed9532
SiBell <SiBellUK@gmail.com>
2020-07-18 13:14:13 +0100

    linkify pipe implemented

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -7,6 +7,7 @@ import { DurationDirective } from './directives/duration.directive';
 import {ObservationModalComponent} from './observation-model/observation-modal.component';
 import {RouterModule} from '@angular/router';
 import {GoogleMapComponent} from './google-maps/google-maps.component';
+import {LinkifyPipe} from './pipes/linkify.pipe';
 
 
 @NgModule({
@@ -14,7 +15,7 @@ import {GoogleMapComponent} from './google-maps/google-maps.component';
   imports: [
     // These are modules that the component(s) in the declarations array need
     CommonModule,
-    RouterModule
+    RouterModule,
   ],
 
   declarations: [
@@ -22,6 +23,7 @@ import {GoogleMapComponent} from './google-maps/google-maps.component';
     DurationDirective,
     ObservationModalComponent, // this is used in more than 1 feature modules, hence loading here.
     GoogleMapComponent,
+    LinkifyPipe
   ],
 
   exports: [
@@ -30,7 +32,8 @@ import {GoogleMapComponent} from './google-maps/google-maps.component';
     DatetimeDirective,
     DurationDirective,
     ObservationModalComponent,
-    GoogleMapComponent
+    GoogleMapComponent,
+    LinkifyPipe
   ]
 
 })
